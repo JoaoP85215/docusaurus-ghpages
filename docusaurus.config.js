a5a0b146854f875735610f676b468ff83e0d74e4
João <a85215@ualg.pt>
2023-11-17 12:10:15 +0000

    Alterei o subtitulo git

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'João Pena',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Dinosaurs are dumb',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
